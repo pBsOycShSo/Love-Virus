299029086a0d57dc7fade6bda6eb06681eb8ea83
Unknown <82444721+LuckyJMax@users.noreply.github.com>
2021-08-19 10:56:35 +0530

    Add files via upload

---
+++ I-love.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#!/Made by LuckyJMax Dark Ti Owner
+#!/Made date 2021/08/19
+#!/Version 1.0
+#!/Please do not copy this tool#p#r#a#n#k
+clear
+echo -e '    \e[1;31mIloveyouIloveyouIloveyouIloveyouIlove'
+echo -e '    \e[1;31mI''\e[1;36m ___ ''\e[1;35m _             ''\e[1;34m               ''\e[1;31mI' 
+echo -e '    \e[1;31ml''\e[1;36m|_ _|''\e[1;35m| |_____ _____ ''\e[1;34m _  _ ___ _  _ ''\e[1;31ml' 
+echo -e '    \e[1;31mo''\e[1;36m | | ''\e[1;35m| / _ \ V / -_)''\e[1;34m| || / _ \ || |''\e[1;31mo' 
+echo -e '    \e[1;31mv''\e[1;36m|___|''\e[1;35m|_\___/\_/\___|''\e[1;34m\_,  \___/\_,_|''\e[1;31mv' 
+echo -e '    \e[1;31me''\e[1;36m     ''\e[1;35m               ''\e[1;34m |__/          ''\e[1;31me' 
+echo -e '    \e[1;31mIloveyouIloveyouIloveyouIloveyouIlove'
+echo -e '                                     \e[1;33mv1.0'
+sleep 1
+echo
+echo -en '\e[1;32mEnter the virus file you want to create : '
+read Vi
+clear
+echo -e '    \e[1;31mIloveyouIloveyouIloveyouIloveyouIlove'
+echo -e '    \e[1;31mI''\e[1;36m ___ ''\e[1;35m _             ''\e[1;34m               ''\e[1;31mI' 
+echo -e '    \e[1;31ml''\e[1;36m|_ _|''\e[1;35m| |_____ _____ ''\e[1;34m _  _ ___ _  _ ''\e[1;31ml' 
+echo -e '    \e[1;31mo''\e[1;36m | | ''\e[1;35m| / _ \ V / -_)''\e[1;34m| || / _ \ || |''\e[1;31mo' 
+echo -e '    \e[1;31mv''\e[1;36m|___|''\e[1;35m|_\___/\_/\___|''\e[1;34m\_,  \___/\_,_|''\e[1;31mv' 
+echo -e '    \e[1;31me''\e[1;36m     ''\e[1;35m               ''\e[1;34m |__/          ''\e[1;31me' 
+echo -e '    \e[1;31mIloveyouIloveyouIloveyouIloveyouIlove'
+echo -e '                                     \e[1;33mv1.0'
+sleep 0.3
+echo -en $Vi '\e[1;32m Where saved this file : '
+read w
+echo
+sleep 1
+echo -e '            \e[1;32m File Saved Successful !!'
+sleep 2
+clear
+echo -e '    \e[1;31mIloveyouIloveyouIloveyouIloveyouIlove'
+echo -e '    \e[1;31mI''\e[1;36m ___ ''\e[1;35m _             ''\e[1;34m               ''\e[1;31mI' 
+echo -e '    \e[1;31ml''\e[1;36m|_ _|''\e[1;35m| |_____ _____ ''\e[1;34m _  _ ___ _  _ ''\e[1;31ml' 
+echo -e '    \e[1;31mo''\e[1;36m | | ''\e[1;35m| / _ \ V / -_)''\e[1;34m| || / _ \ || |''\e[1;31mo' 
+echo -e '    \e[1;31mv''\e[1;36m|___|''\e[1;35m|_\___/\_/\___|''\e[1;34m\_,  \___/\_,_|''\e[1;31mv' 
+echo -e '    \e[1;31me''\e[1;36m     ''\e[1;35m               ''\e[1;34m |__/          ''\e[1;31me' 
+echo -e '    \e[1;31mIloveyouIloveyouIloveyouIloveyouIlove'
+echo -e '                                     \e[1;33mv1.0'
+sleep 0.3
+cd
+echo -en '\e[1;32mDo you like this tool (yes/no) : '
+read y
+clear
+echo -e '    \e[1;31mIloveyouIloveyouIloveyouIloveyouIlove'
+echo -e '    \e[1;31mI''\e[1;36m ___ ''\e[1;35m _             ''\e[1;34m               ''\e[1;31mI' 
+echo -e '    \e[1;31ml''\e[1;36m|_ _|''\e[1;35m| |_____ _____ ''\e[1;34m _  _ ___ _  _ ''\e[1;31ml' 
+echo -e '    \e[1;31mo''\e[1;36m | | ''\e[1;35m| / _ \ V / -_)''\e[1;34m| || / _ \ || |''\e[1;31mo' 
+echo -e '    \e[1;31mv''\e[1;36m|___|''\e[1;35m|_\___/\_/\___|''\e[1;34m\_,  \___/\_,_|''\e[1;31mv' 
+echo -e '    \e[1;31me''\e[1;36m     ''\e[1;35m               ''\e[1;34m |__/          ''\e[1;31me' 
+echo -e '    \e[1;31mIloveyouIloveyouIloveyouIloveyouIlove'
+echo -e '                                     \e[1;33mv1.0'
+sleep 0.3
+echo -en '\e[1;32mPlease tell me Do you like this tool (yes/no) : '
+read e
+clear
+echo -e '    \e[1;31mIloveyouIloveyouIloveyouIloveyouIlove'
+echo -e '    \e[1;31mI''\e[1;36m ___ ''\e[1;35m _             ''\e[1;34m               ''\e[1;31mI' 
+echo -e '    \e[1;31ml''\e[1;36m|_ _|''\e[1;35m| |_____ _____ ''\e[1;34m _  _ ___ _  _ ''\e[1;31ml' 
+echo -e '    \e[1;31mo''\e[1;36m | | ''\e[1;35m| / _ \ V / -_)''\e[1;34m| || / _ \ || |''\e[1;31mo' 
+echo -e '    \e[1;31mv''\e[1;36m|___|''\e[1;35m|_\___/\_/\___|''\e[1;34m\_,  \___/\_,_|''\e[1;31mv' 
+echo -e '    \e[1;31me''\e[1;36m     ''\e[1;35m               ''\e[1;34m |__/          ''\e[1;31me' 
+echo -e '    \e[1;31mIloveyouIloveyouIloveyouIloveyouIlove'
+echo -e '                                     \e[1;33mv1.0'
+sleep 0.3
+echo -en '\e[1;32mPleeeease tell me Do you like this tool (yes/no) : '
+read s
+clear
+sleep 5
+echo
+echo
+echo
+echo -en '    \e[1;31m!!!!You made a mistake!!!! You have 5 more minutes'
+sleep 5
+yes $Vi $y $e $s $Vi $y $e $s $Vi $y $e $s $Vi $y $e $s $Vi $y $e $s
